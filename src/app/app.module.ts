f0bec84ac3b81c587619b368a51e4c81a7c240a6
Yang Li <li.yang@casstime.com>
2020-09-18 19:42:03 +0800

    hero list and detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
